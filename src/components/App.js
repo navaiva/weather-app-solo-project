0750f907232d4fe4c30aec5eed519d0744710e0f
root <root@LAPTOP-ADJ5460M>
2024-04-25 13:49:16 -0700

    work

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,133 @@
-import React from "react";
-import sunny from '../assets/sunny.jpeg';
-import windy from '../assets/windy.jpeg';
+import React, { useState } from 'react';
+import sunny from '../assets/sunny-day.jpg'
+import mist from '../assets/forest.jpg';
 import snowy from '../assets/snowy.jpeg';
 import rainy from '../assets/rainy.jpg';
 import lighting from '../assets/lightingstorm.jpeg';
 import cloudy from '../assets/sky-clouds-4k-ja.jpg';
-import { useEffect } from "react";
-import { getWeatherData } from "./weather";
-
-
-
-
-
+import { useEffect } from 'react';
+import { getWeatherData } from './weather';
 
 
 function App() {
-    
-        useEffect(() => {
-        const fetchedData = async () => {
-            const data = await getWeatherData('paris')
-       
-        };
-        fetchedData()
-    }, []);
+  const [city, setCity] = useState('paris');
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('imperial');
+  const [backgroundImg, setbackgroundImg] = useState(sunny);
 
+  useEffect(() => {
+    const fetchedData = async () => {
+      const data = await getWeatherData(city, units);
+      setWeather(data);
 
-        const backgroundStyle = {
-            backgroundImage: `url(${snowy})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          };
-        return (
-            
-            <div className="app" style={backgroundStyle}>
-                {/* <img className='image' src={cloudy} alt=""></img> */}
-               <div className='overlay'>
-                <div className="container">
-                    <div className="section sec__inp">
-                        <input type="text" name="city"
-                        placeholder="entercity"/>
-                        <button>°F</button>
-                    </div>
-                    <div className='section sec__temp'>
-                        <div className="icon">
-                            <h3>london</h3>
-                            <img src="" alt="weather icon" />
-                            <h3>sunny</h3>
-                        </div>
-                        <div className="temperature">
-                            <h1> 34°F</h1>
-                        </div>
-                    </div>
-                    {/* eeeeeeeeee */}
-                </div>
-               </div>
+      console.log(data.description);
 
+      if (
+        data.description === 'few clouds' ||
+        data.description === 'clear sky'
+      ) {
+        setbackgroundImg(sunny);
+      }
+
+      if (
+        data.description === 'overcast clouds' ||
+        data.description === 'broken clouds'
+      ) {
+        setbackgroundImg(cloudy);
+      }
+
+      if (data.description === 'shower rain' || data.description === 'rain') {
+        setbackgroundImg(rainy);
+      }
+
+      if (data.description === 'thunderstorm') {
+        setbackgroundImg(lighting);
+      }
+
+      if (data.description === 'snow') {
+        setbackgroundImg(snowy);
+      }
+
+      if (data.description === 'mist') {
+        
+        setbackgroundImg(mist)
+               }
+    };
+    fetchedData();
+  }, [units, city]);
+
+  const handleUnits = (e) => {
+    const button = e.currentTarget;
+    const currUnit = button.innerText.slice(1);
+
+    const cel = currUnit === 'C';
+    button.innerText = cel ? '°F' : '°C';
+    setUnits(cel ? 'metric' : 'imperial');
+  };
+
+  const enterInp = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      console.log(e.currentTarget.value);
+      e.currentTarget.blur();
+      e.currentTarget.value = ''
+    }
+  };
+
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+  return (
+    <div className="app" style={backgroundStyle}>
+      {/* <img className='image' src={cloudy} alt=""></img> */}
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section sec__inp">
+              <input
+                onKeyDown={enterInp}
+                type="text"
+                name="city"
+                placeholder="entercity"
+              />
+              <button onClick={(e) => handleUnits(e)}> °C</button>
             </div>
-        )
+            <div className="section sec__temp">
+              <div className="icon">
+                <h3>{` ${weather.name}, ${weather.country} `}</h3>
+                <img src={weather.iconUrl} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === 'metric' ? 'C' : 'F'
+                }`}</h1>
+              </div>
+              
+            </div>
+            <div className='max'>
+                <small>max</small>
+                <h1>{`${weather.temp_max.toFixed()} °${
+                  units === 'metric' ? 'C' : 'F' }`}</h1>
+              </div>
+              <div className='min'>
+                <small>min</small>
+                <h1>{`${weather.temp_min.toFixed()} °${
+                  units === 'metric' ? 'C' : 'F' }`}</h1>
+              </div>
+              <div className='wind'>
+                <small>wind</small>
+                <h1>{`${weather.speed.toFixed()} ${
+                  units === 'metric' ? 'm/s' : 'm/h' }`}</h1>
+              </div>
+           
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,6 @@
 const key = 
 
-const makeIcon = ''
+const makeIcon = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
 
 const getWeatherData = async (city, units = 'metric') => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`
@@ -8,6 +8,8 @@ const getWeatherData = async (city, units = 'metric') => {
     const data = await fetch(url)
     .then((res) => res.json())
     .then((data) => data);
+    console.log(data)
+
   
 
 const {weather, main: {temp, temp_min, temp_max},
@@ -18,7 +20,7 @@ name,  } = data
 const {description, icon} = weather[0]
 
 return {
-description, icon, temp, temp_min, temp_max, speed, country, name
+description, iconUrl: makeIcon(icon), temp, temp_min, temp_max, speed, country, name
 }
 };
 
